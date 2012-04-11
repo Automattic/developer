0f4ed74725a0df0ddc5664b9d082a836229d33d6
Nikolay Bachiyski <nb@nikolay.bg>
2012-04-11 16:23:30 +0300

    Add a LICENSE file, instead of cluttering the plugin file

--- developer.php
+++ developer.php
@@ -11,32 +11,7 @@ Author URI:   http://automattic.com/
 
 Text Domain:  a8c-developer
 Domain Path:  /localization/
-
-**************************************************************************
-
-Copyright (C) 2012 Automattic
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 or greater,
-as published by the Free Software Foundation.
-
-You may NOT assume that you can use any other version of the GPL.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-The license for this software can likely be found here:
-http://www.gnu.org/licenses/gpl-2.0.html
-
-**************************************************************************
-
-TODO:
-
-* PHPdoc. Will be done very, very last.
-
-**************************************************************************/
+*/
 
 class Automattic_Developer {
 
